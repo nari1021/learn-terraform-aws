4d4850a50ce116ee1d726306233b38e0654ec9d4
nari1021 <y.shhh000@gmail.com>
2021-10-25 16:29:27 +0900

    create :: AWS EC2 배포

---
+++ learn-terraform-aws-instance/main.tf
@@ -0,0 +1,23 @@
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 3.27"
+        }
+    }
+    required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+    profile = "default"
+    region  = "ap-northeast-2"
+}
+
+resource "aws_instance" "app_server" {
+    ami = "ami-0e4a9ad2eb120e054"
+    instance_type = "t2.micro"
+    
+    tags = {
+        Name = "ExampleAppServerInstance"
+    }
+}
