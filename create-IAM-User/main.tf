1815753db7e08c562459bb5315817ce30c54510d
nari1021 <y.shhh000@gmail.com>
2021-10-28 11:04:27 +0900

    create :: 생성한 IAM user를 새롭게 생성한 IAM group에 등록시킴

--- create-IAM-User/main.tf
+++ create-IAM-User/main.tf
@@ -11,3 +11,14 @@ resource "aws_iam_user" "nari120" {
 resource "aws_iam_group" "terraform_group" {
     name = "terraform_test_group"
 }
+
+# IAM user -> IAM group에 등록
+resource "aws_iam_group_membership" "terraforms" {
+    name = aws_iam_group.terraform_group.name
+
+    users = [
+        aws_iam_user.nari120.name
+    ]
+
+    group = aws_iam_group.terraform_group.name
+}
