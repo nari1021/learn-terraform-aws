1b82ed995da34dfeed23ac5526381732091cb464
nari1021 <y.shhh000@gmail.com>
2021-10-28 10:59:24 +0900

    create :: iam_group 깡통 생성

--- create-IAM-User/main.tf
+++ create-IAM-User/main.tf
@@ -2,6 +2,12 @@ provider "aws" {
     region = "ap-northeast-2"
 }
 
+# IAM User 생성
 resource "aws_iam_user" "nari120" {
     name = "nari120"
+}
+
+# IAM Group 생성
+resource "aws_iam_group" "terraform_group" {
+    name = "terraform_test_group"
 }
